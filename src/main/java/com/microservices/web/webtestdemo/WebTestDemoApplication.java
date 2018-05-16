e3effafaa8646be32fea09658c604a671c611e8c
Kaushal <mrkaish@gmail.com>
2018-05-16 21:55:02 +0530

    spring boot sample examples

---
+++ src/main/java/com/microservices/web/webtestdemo/WebTestDemoApplication.java
@@ -0,0 +1,12 @@
+package com.microservices.web.webtestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTestDemoApplication.class, args);
+	}
+}
